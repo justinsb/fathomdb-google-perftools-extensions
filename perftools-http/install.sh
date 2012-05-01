5bbe80454b8db70c5e0a0243eb30db219d3cafc8
Justin Santa Barbara <justin@fathomdb.com>
2012-05-01 02:13:43 -0700

    Install static libraries (*.a)

--- perftools-http/install.sh
+++ perftools-http/install.sh
@@ -2,7 +2,7 @@
 
 rsync -r --links --verbose --times --include="*.h" --include="*/" --exclude="*" src/main/cpp/fathomdb/ /usr/local/include/fathomdb/
 
-rsync -r --verbose --times --include="*.so" --exclude="*" bin/ /usr/local/lib/
+rsync -r --verbose --times --include="*.so" --include="*.a" --exclude="*" bin/ /usr/local/lib/
 
 
 
